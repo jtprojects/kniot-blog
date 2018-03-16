eedf7250618854d612c56e904d7d0c395ec94f95
Jonas <jonas.trezl@web.de>
2018-03-16 23:48:57 +0000

    allowhosts

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'i-9pcu6%2hhh5f@5h97@(06+$1z(p^w-jh$yh5d)#phi_94-ww'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.15', 'localhost']
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
